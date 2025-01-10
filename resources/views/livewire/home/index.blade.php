68234a5ac0dd63f3be803abf1c84ccc1511bc5ec
fathurwalkers44 <fathurwalkers44@gmail.com>
2025-01-09 16:11:44 -0800

    update

--- resources/views/livewire/home/index.blade.php
+++ resources/views/livewire/home/index.blade.php
@@ -1,3 +1,38 @@
 <div>
-    Home: A good traveler has no fixed plans and is not intent upon arriving.
+    <div class="row mt-4 mb-2">
+        <div class="col-sm-6 col-md-6 col-lg-6">
+            <h2>row 1</h2>
+        </div>
+        <div class="col-sm-6 col-md-6 col-lg-6">
+            <table class="table table-bordered">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+              </table>
+        </div>
+    </div>
 </div>
