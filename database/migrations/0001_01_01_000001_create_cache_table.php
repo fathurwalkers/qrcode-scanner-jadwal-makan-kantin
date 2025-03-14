fef2641020c279a48625e39f73ff762e23fdd46c
fathurwalkers44 <fathurwalkers44@gmail.com>
2025-03-14 09:35:11 -0700

    update

--- database/migrations/0001_01_01_000001_create_cache_table.php
+++ database/migrations/0001_01_01_000001_create_cache_table.php
@@ -8,22 +8,22 @@ return new class extends Migration
 {
     public function up(): void
     {
-        // Schema::create('cache', function (Blueprint $table) {
-        //     $table->string('key')->primary();
-        //     $table->mediumText('value');
-        //     $table->integer('expiration');
-        // });
+        Schema::create('cache', function (Blueprint $table) {
+            $table->string('key')->primary();
+            $table->mediumText('value');
+            $table->integer('expiration');
+        });
 
-        // Schema::create('cache_locks', function (Blueprint $table) {
-        //     $table->string('key')->primary();
-        //     $table->string('owner');
-        //     $table->integer('expiration');
-        // });
+        Schema::create('cache_locks', function (Blueprint $table) {
+            $table->string('key')->primary();
+            $table->string('owner');
+            $table->integer('expiration');
+        });
     }
 
     public function down(): void
     {
-        // Schema::dropIfExists('cache');
-        // Schema::dropIfExists('cache_locks');
+        Schema::dropIfExists('cache');
+        Schema::dropIfExists('cache_locks');
     }
 };
